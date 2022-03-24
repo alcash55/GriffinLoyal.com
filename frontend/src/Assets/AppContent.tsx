5715f855e67d097437695a5344c824448336bf37
Alex Cash <alex.e.cash28@gmail.com>
2022-03-24 01:23:20 -0400

    added Merch.tsx to AppContent

--- frontend/src/Assets/AppContent.tsx
+++ frontend/src/Assets/AppContent.tsx
@@ -1,8 +1,11 @@
 import { Box, Paper, Typography, Divider } from '@mui/material';
+import Merch from './content/Merch.tsx';
 const AppContent = () => {
   return (
     <Paper elevation={3}>
       <Box>
+        <Merch />
+        <Divider />
         <Typography>This is where content goes</Typography>
         <Divider />
         <Typography>This is where content goes</Typography>
---
+++ frontend/src/Assets/content/Merch.tsx
@@ -0,0 +1,6 @@
+import { Typography } from '@mui/material';
+const Merch = () => {
+  return <Typography>This is where the merch goes</Typography>;
+};
+
+export default Merch;
