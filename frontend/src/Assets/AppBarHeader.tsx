30a73b2f630399ab39ac7e138925fb9c1692097d
Alex Cash <alex.e.cash28@gmail.com>
2022-03-24 01:22:45 -0400

    Changed button type in AppBarHeader

--- frontend/src/Assets/AppBarHeader.tsx
+++ frontend/src/Assets/AppBarHeader.tsx
@@ -1,42 +1,86 @@
-import { Box, AppBar, Container, Toolbar, IconButton } from '@mui/material';
+import { Box, AppBar, Container, Toolbar, IconButton, SvgIcon, Button } from '@mui/material';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-// import { TwitterIcon } from '@mui/icons-material';
+// import { TwitterIcon, InstagramIcon, YouTubeIcon } from '@mui/material';
 import Loyal from './images/Loyal.svg';
 
 const AppBarHeader = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#682392' }}>
+    <AppBar position="static" sx={{ backgroundColor: '#682392', p: 2 }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters sx={{ alignItems: 'center', justifyContent: 'center' }}>
-          <Box display="flex" flexDirection="row" sx={{ p: 3 }}>
-            <IconButton sx={{ ml: 3, mr: 3, backgroundColor: '#8D32C3', color: '#E5B13E' }}>
-              Merch
-            </IconButton>
-            <IconButton sx={{ ml: 3, mr: 3, backgroundColor: '#8D32C3', color: '#E5B13E' }}>
-              Sponsors
-            </IconButton>
+          <Box display="contents" flexDirection="row" sx={{ p: 3 }}>
+            <Button
+              sx={{
+                ml: 3,
+                mr: 3,
+                backgroundColor: '#8D32C3',
+                color: '#E5B13E',
+                minWidth: '5em',
+                maxHeight: '3em',
+              }}
+            >
+              Schedule
+            </Button>
+            <Button
+              sx={{
+                ml: 3,
+                mr: 3,
+                backgroundColor: '#8D32C3',
+                color: '#E5B13E',
+                minWidth: '5em',
+                maxHeight: '3em',
+              }}
+            >
+              Watch
+            </Button>
             <Box sx={{ display: 'block', ml: 5, mr: 5 }}>
               <img src={Loyal} alt="GB Logo" />
               <Box>
                 <IconButton>
-                  <TwitterIcon />
+                  <SvgIcon>
+                    <TwitterIcon />
+                  </SvgIcon>
                 </IconButton>
+
                 <IconButton>
-                  <InstagramIcon />
+                  <SvgIcon>
+                    <InstagramIcon />
+                  </SvgIcon>
                 </IconButton>
+
                 <IconButton>
-                  <YouTubeIcon />
+                  <SvgIcon>
+                    <YouTubeIcon />
+                  </SvgIcon>
                 </IconButton>
               </Box>
             </Box>
-            <IconButton sx={{ ml: 3, mr: 3, backgroundColor: '#8D32C3', color: '#E5B13E' }}>
-              Watch
-            </IconButton>
-            <IconButton sx={{ ml: 3, mr: 3, backgroundColor: '#8D32C3', color: '#E5B13E' }}>
-              Schedule
-            </IconButton>
+            <Button
+              sx={{
+                ml: 3,
+                mr: 3,
+                backgroundColor: '#8D32C3',
+                color: '#E5B13E',
+                minWidth: '5em',
+                maxHeight: '3em',
+              }}
+            >
+              Merch
+            </Button>
+            <Button
+              sx={{
+                ml: 3,
+                mr: 3,
+                backgroundColor: '#8D32C3',
+                color: '#E5B13E',
+                minWidth: '5em',
+                maxHeight: '3em',
+              }}
+            >
+              Sponsors
+            </Button>
           </Box>
         </Toolbar>
       </Container>
