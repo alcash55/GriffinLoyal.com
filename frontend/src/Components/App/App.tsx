3d297c8c06947a72b8e46c21aa973fee5a424979
jepsenzd <zjepsen98@yahoo.com>
2022-03-25 17:37:08 -0400

    Added animated background

--- frontend/src/Components/App/App.tsx
+++ frontend/src/Components/App/App.tsx
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <SideBar />
-      {/* <AppContent />
-      <Footer /> */}
+      <AppContent />
     </div>
   );
 }
--- frontend/src/Components/AppShell/AppContent/AppContent.tsx
+++ frontend/src/Components/AppShell/AppContent/AppContent.tsx
@@ -1,9 +1,18 @@
 import { Box, Paper, Typography, Divider } from "@mui/material";
+import "./AppContent.scss";
 // import Merch from './sections/Merch.tsx';
 const AppContent = () => {
   return (
     <Paper elevation={3}>
       <Box>
+        <div className="d-flex flex-column justify-content-center w-100 h-100">
+          <a
+            href="http://twitch.tv/griffinloyal"
+            className="text-decoration-none"
+          >
+            <h5 className="fw-light text-white m-0">— Watch the Stream —</h5>
+          </a>
+        </div>
         {/* <Merch /> */}
         <Divider />
         <Typography>This is where content goes</Typography>
