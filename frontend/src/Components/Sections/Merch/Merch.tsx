5fbbc87f83b4e229648b388ad827ba4a7edbd31a
Alex Cash <alex.e.cash28@gmail.com>
2022-05-09 00:48:30 -0400

    fixed height added more cards

--- frontend/src/Components/Sections/Merch/Merch.tsx
+++ frontend/src/Components/Sections/Merch/Merch.tsx
@@ -4,7 +4,6 @@ import {
   Typography,
   Card,
   CardMedia,
-  CardContent,
   Button,
   CardActions,
   CardActionArea,
@@ -22,7 +21,58 @@ const Merch = () => {
             <CardActionArea>
               <CardMedia
                 component="img"
-                height="140"
+                height="100%"
+                image={Shirt}
+                alt="Shirt SVG"
+              />
+            </CardActionArea>
+            <Typography variant="h6">T-Shirt</Typography>
+            <CardActions>
+              <Button variant="outlined">Buy Now</Button>
+              <Button variant="outlined">More Information</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="100%"
+                image={Shirt}
+                alt="Shirt SVG"
+              />
+            </CardActionArea>
+            <Typography variant="h6">T-Shirt</Typography>
+            <CardActions>
+              <Button variant="outlined">Buy Now</Button>
+              <Button variant="outlined">More Information</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="100%"
+                image={Shirt}
+                alt="Shirt SVG"
+              />
+            </CardActionArea>
+            <Typography variant="h6">T-Shirt</Typography>
+            <CardActions>
+              <Button variant="outlined">Buy Now</Button>
+              <Button variant="outlined">More Information</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="100%"
                 image={Shirt}
                 alt="Shirt SVG"
               />
