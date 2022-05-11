8dc2fef8fc0d5082e9a590997d640259b4595b57
Alex Cash <alex.e.cash28@gmail.com>
2022-05-11 00:39:39 -0400

    changed app and index

--- frontend/src/Components/App/App.tsx
+++ frontend/src/Components/App/App.tsx
@@ -2,13 +2,11 @@ import AppContent from "../AppShell/AppContent/AppContent";
 import SideBar from "../AppShell/Sidebar/SideBar";
 import "./App.scss";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <SideBar />
       <AppContent />
     </div>
   );
-}
-
-export default App;
+};
