de424c404d9a06f4e95c9b4191acee9a7a526a69
Alex Cash <alex.e.cash28@gmail.com>
2022-05-13 02:46:11 -0400

    added twitch iframe

--- frontend/src/Components/Sections/Watch/Watch.tsx
+++ frontend/src/Components/Sections/Watch/Watch.tsx
@@ -1,24 +1,26 @@
-import { Grid, Box, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import "./Watch.scss";
 
 const Watch = () => {
   return (
-    <Box sx={{ flexGrow: 1 }} className="watch">
+    <Box className="watch">
       <Typography variant="h4">Watch</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-      </Grid>
+      <Box
+        sx={{ display: "flex", justifyContent: "space-around", mr: 35, pt: 15 }}
+      >
+        {/* https://dev.twitch.tv/docs/embed/video-and-clips/ */}
+        {/* Possibly doesnt work with local host */}
+        <iframe
+          className="embedStream"
+          title="Griffin Loyal's Twitch"
+          src="https://player.twitch.tv/?channel=GriffinLoyal&parent=localhost:3000&autoplay=true&muted=true"
+          frameBorder="0"
+          allowFullScreen={true}
+          scrolling="no"
+          height="720"
+          width="1280"
+        ></iframe>
+      </Box>
     </Box>
   );
 };
