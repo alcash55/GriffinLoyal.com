24190f0b1ce981d339279a761dfe18eef5b831c2
Alex Cash <alex.e.cash28@gmail.com>
2022-05-19 03:24:08 -0400

    added spotify and soundlcoud iframes

--- frontend/src/Components/Sections/Music/Music.tsx
+++ frontend/src/Components/Sections/Music/Music.tsx
@@ -1,24 +1,47 @@
-import { Grid, Box, Typography } from "@mui/material";
+import { Box, Typography, Divider } from "@mui/material";
 import "./Music.scss";
 
 const Music = () => {
   return (
-    <Box sx={{ flexGrow: 1 }} className="schedule">
+    <Box sx={{}} className="schedule">
       <Typography variant="h4">Music</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography>This is where content goes</Typography>
-        </Grid>
-      </Grid>
+      <Divider />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ p: 1, width: "100%" }}>
+          <iframe
+            title="GBIB Spotify"
+            className="spotify"
+            src="https://open.spotify.com/embed/artist/6Y4TcwRvIaudJHYpsdBsR4?utm_source=generator"
+            width="50%"
+            height="380"
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          ></iframe>
+        </Box>
+        <Box
+          sx={{
+            p: 1,
+            width: "100%",
+          }}
+        >
+          <iframe
+            title="GBIB Soundcloud"
+            width="50%"
+            height="166"
+            scrolling="no"
+            frameBorder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/912885256&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          ></iframe>
+        </Box>
+      </Box>
     </Box>
   );
 };
